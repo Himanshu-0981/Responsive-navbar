b83338201a591ffb493ab01ecbf3bd067d4e634b
Himanshu <kumarhimanshu2219@gmail.com>
2022-06-20 15:35:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let header = document.querySelector('.header');
+let toggleBtn = document.querySelector('.nav-toggle-btn');
+let open = document.querySelector('.open');
+
+
+
+
+toggleBtn.addEventListener('click',toggleFunciton = () => {
+    header.classList.toggle('active');
+    console.log('toggle btn clicked');
+});
